ee7a2081f5ae1335a1cd4f63176a06f2d0048b1d
Markus Ressel <mail@markusressel.de>
2019-10-11 22:13:21 +0200

    added regex test drop python 3.5 support

--- tests/entry_test.py
+++ tests/entry_test.py
@@ -66,6 +66,7 @@ class EntryTest(EntryTestBase):
         input_output = [
             ("$[0-9]*", re.compile("$[0-9]*", re.IGNORECASE)),
             (re.compile("$[0-9]*", re.IGNORECASE), re.compile("$[0-9]*", re.IGNORECASE)),
+            (re.compile("$[0-9]*"), ValueError),
         ]
 
         self.assert_input_output(config_entry, input_output)
